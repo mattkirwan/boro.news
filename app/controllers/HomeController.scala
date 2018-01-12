f8d1b7b2f792851fc8fca2844f5765cab57ee089
Matt Kirwan <matthew.kirwan@skybettingandgaming.com>
2018-01-12 15:48:59 +0000

    Added rss request and title loop

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,8 +1,11 @@
 package controllers
 
 import javax.inject._
+
 import play.api._
 import play.api.mvc._
+
+
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -19,6 +22,29 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
    * a path of `/`.
    */
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+
+    val responseString: String = get("http://www.gazettelive.co.uk/all-about/middlesbrough-fc?service=rss")
+
+    val xml = scala.xml.XML.loadString(responseString)
+    val rss: String = xml.mkString
+
+    val articleTitles = for {
+      t <- xml \\ "item" \ "title"
+    } yield t.text
+
+    Ok(views.html.index(articleTitles))
   }
+
+  def get(url: String)  = scala.io.Source.fromURL(url).mkString
 }
+
+
+
+//object MyHttp extends Http {
+//  proxyConfig = None,
+//  options = HttpConstants.defaultOptions,
+//  charset = HttpConstants.utf8,
+//  sendBufferSize = 4096,
+//  userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:57.0) Gecko/20100101 Firefox/57.0",
+//  compress = true
+//}
--- build.sbt
+++ build.sbt
@@ -7,6 +7,9 @@ lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 scalaVersion := "2.12.3"
 
+libraryDependencies += "org.scalaj" %% "scalaj-http" % "2.3.0"
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "1.0.6"
+
 libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "3.1.2" % Test
 
