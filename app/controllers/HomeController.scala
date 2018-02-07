79a731127a8ea01e4eb50adcff9461e45be0338e
Matt Kirwan <matthew.kirwan@skybettingandgaming.com>
2018-02-07 10:18:52 +0000

    Added some basic checking around existing request data

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -3,6 +3,7 @@ package controllers
 import java.io.{File, PrintWriter}
 import java.text.SimpleDateFormat
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.Date
 import javax.inject._
 
@@ -25,42 +26,54 @@ import scala.util.matching.Regex.Match
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+
+
+  def fetchLatestGazetteArticles(): String = {
+    scala.io.Source.fromURL("https://www.gazettelive.co.uk/all-about/middlesbrough-fc?service=rss").mkString
+  }
+
+  def storeResponse(resp: String) = {
+    val gazetteArticlesFileName: String = new SimpleDateFormat("'./data/gazette/'yyyyMMddHHmm'.txt'").format(new Date())
+    val writer = new PrintWriter(new File(gazetteArticlesFileName))
+    writer.write(resp)
+  }
+
+  def fetchNewArticle(d: LocalDateTime): Boolean = {
+    if (LocalDateTime.now().isAfter(d.plusHours(2))) {
+      return true
+    }
+    false
+  }
+
   def index() = Action { implicit request: Request[AnyContent] =>
 
-    // Evening Gazette
-    val gazetteResponse: String = scala.io.Source.fromURL("https://www.gazettelive.co.uk/all-about/middlesbrough-fc?service=rss").mkString
+    val gazetteFiles: Array[File] = new File("./data/gazette").listFiles()
 
+    if (gazetteFiles.isEmpty) {
+      println("There are no files...updating")
+      // Need to sort something out for this async call because getting latestFileName will obviously fail
+      storeResponse(fetchLatestGazetteArticles())
+    }
 
+    val latestFileName = gazetteFiles
+      .map(f => "[0-9]+".r findFirstIn f.toString )
+      .map(f => {f.get.toLong})
+      .max.toString
 
+    val latestFileTime: LocalDateTime = LocalDateTime.parse(latestFileName, DateTimeFormatter.ofPattern("yyyyMMddHHmm"))
 
-    val gazetteArticlesFileName: String = new SimpleDateFormat("'./data/'yyyyMMddHHmm'_gazette.txt'").format(new Date())
+    val update: Boolean = LocalDateTime.now().isAfter(latestFileTime.plusMinutes(30))
 
-    val writer = new PrintWriter(new File(gazetteArticlesFileName))
-
-    writer.write(gazetteResponse)
-
-    val data = new File("./data").listFiles()
-      .map(f => {
-        "20180205211".r findFirstIn(f.toString)
-      })
-      .map(f => { println(f) })
-
-
-    println(LocalDateTime.now().minusHours(12))
-
+    if (update) {
+      println("Updating latest gazette articles...")
+      storeResponse(fetchLatestGazetteArticles())
+    }
 
     def parseXML(xml: String): Try[scala.xml.Elem] = {
       Try(scala.xml.XML.loadString(xml))
     }
 
-    val gazetteArticles = parseXML(gazetteResponse) match {
+    val gazetteArticles = parseXML(latestFileName) match {
       case Success(out) => for {
         a <- out \\ "item"
         t <- a \ "title"
@@ -95,7 +108,7 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
     val beep = skySportsResponse
 
 
-    Ok(views.html.index(articles, gazetteArticlesFileName))
+    Ok(views.html.index(articles, ""))
 
   }
 
